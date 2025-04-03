7cd1027c5a4d70c78d3d746f936d1b63124906e6
Mikuláš Šťastný <mikulas.stastny@bringauto.com>
2025-04-03 13:53:19 +0200

    Remove any_machine flag

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -1,8 +1,8 @@
 SET(CMAKE_FIND_USE_CMAKE_SYSTEM_PATH FALSE)
 
-BA_PACKAGE_LIBRARY(fleet-protocol-interface v2.0.0 PLATFORM_STRING_MODE any_machine NO_DEBUG ON)
+BA_PACKAGE_LIBRARY(fleet-protocol-interface v2.0.0 NO_DEBUG ON)
 BA_PACKAGE_LIBRARY(protobuf                 v4.21.12)
 BA_PACKAGE_LIBRARY(zlib                     v1.2.11)
 IF (BRINGAUTO_SAMPLES)
-    BA_PACKAGE_LIBRARY(nlohmann-json v3.10.5 PLATFORM_STRING_MODE any_machine NO_DEBUG ON)
+    BA_PACKAGE_LIBRARY(nlohmann-json v3.10.5 NO_DEBUG ON)
 endif()
