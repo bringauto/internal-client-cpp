7a62448f09aef456d88af309086ff4d94a289d13
jiriskuta <jiri.skuta@bringauto.com>
2026-06-10 12:49:53 +0200

    bump fleet-protocol-interface to v2.1.0 (#11)

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -1,6 +1,6 @@
 SET(CMAKE_FIND_USE_CMAKE_SYSTEM_PATH FALSE)
 
-BA_PACKAGE_LIBRARY(fleet-protocol-interface v2.0.0 NO_DEBUG ON)
+BA_PACKAGE_LIBRARY(fleet-protocol-interface v2.1.0 NO_DEBUG ON)
 BA_PACKAGE_LIBRARY(protobuf                 v4.21.12)
 BA_PACKAGE_LIBRARY(zlib                     v1.2.11)
 IF (BRINGAUTO_SAMPLES)
